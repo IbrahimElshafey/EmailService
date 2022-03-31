8dad28b19b582e72f0720f6874d27f51240a5af1
IKhalif-NRRC <ikhalif@nrrc.gov.sa>
2022-03-31 15:34:43 +0300

    init

---
+++ EmailService.Core/IEmailSender.cs
@@ -0,0 +1,11 @@
+﻿using EmailService.Core.Contracts;
+using System;
+using System.Threading.Tasks;
+
+namespace EmailService.Core
+{
+    public interface IEmailSender
+    {
+        Task<bool> SendEmail(EmailMessage message);
+    }
+}
